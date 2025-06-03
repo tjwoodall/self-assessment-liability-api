9441f15ce2f5b6ec595486972cf58b6033461e50
alikouhestani <90757950+alikouhestani@users.noreply.github.com>
2025-06-03 20:07:27 +0100

    WIP

--- app/controllers/AuthenticateRequestController.scala
+++ app/controllers/AuthenticateRequestController.scala
@@ -16,13 +16,13 @@
 
 package controllers
 
-
 import config.AppConfig
-import models.RequestData
-import play.api.mvc.{ActionBuilder, AnyContent, BodyParser, ControllerComponents, Request, Result}
-import uk.gov.hmrc.auth.core.AffinityGroup.Individual
+import models.{ApiErrorResponses, RequestData}
+import play.api.mvc.*
+import uk.gov.hmrc.auth.core.AffinityGroup.{Agent, Individual, Organisation}
 import uk.gov.hmrc.auth.core.authorise.Predicate
-import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisedFunctions, Enrolment}
+import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals.{affinityGroup, authorisedEnrolments}
+import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisationException, AuthorisedFunctions, Enrolment}
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
@@ -30,33 +30,64 @@ import javax.inject.Singleton
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
-class AuthenticateRequestController (cc: ControllerComponents, override val authConnector: AuthConnector)(implicit appConfig: AppConfig, ec: ExecutionContext)
-        extends BackendController(cc) with AuthorisedFunctions{
-  
-  def authorisedAction(utr: String):ActionBuilder[RequestData, AnyContent] = new ActionBuilder[RequestData, AnyContent]{
-    override def parser: BodyParser[AnyContent] = cc.parsers.defaultBodyParser
+class AuthenticateRequestController(
+    cc: ControllerComponents,
+    override val authConnector: AuthConnector
+)(implicit appConfig: AppConfig, ec: ExecutionContext)
+    extends BackendController(cc)
+    with AuthorisedFunctions {
 
-    override protected def executionContext: ExecutionContext = cc.executionContext
+  def authorisedAction(utr: String): ActionBuilder[RequestData, AnyContent] =
+    new ActionBuilder[RequestData, AnyContent] {
+      override def parser: BodyParser[AnyContent] = cc.parsers.defaultBodyParser
 
+      override protected def executionContext: ExecutionContext = cc.executionContext
 
-    override def invokeBlock[A](request: Request[A], block: RequestData[A] => Future[Result]): Future[Result] = {
-      implicit val headerCarrier: HeaderCarrier = hc(request)
+      override def invokeBlock[A](
+          request: Request[A],
+          block: RequestData[A] => Future[Result]
+      ): Future[Result] = {
+        implicit val headerCarrier: HeaderCarrier = hc(request)
 
-      authorised(selfAssessmentEnrolments(utr)) {
-       block(RequestData(utr,request))
+        authorised(selfAssessmentEnrolments(utr)) {
+          block(RequestData(utr, None, request))
+        }
+          .recoverWith { case _: AuthorisationException =>
+            val mtdId: Future[String] = ???
+            mtdId.flatMap { id =>
+              authorised(checkForMtdEnrolment(id)).retrieve(affinityGroup) {
+                case Some(Individual) =>
+                  block(RequestData(utr, None, request))
+                case Some(Organisation)=>
+                  block(RequestData(utr, None, request))
+                case Some(Agent)=>
+                  authorised(agentDelegatedEnrolments(utr, id)) {
+                    block(RequestData(utr, None, request))
+                  }
+                case _ => Future.failed(ApiErrorResponses.apply(500, "unsupported affinity group"))
+              }
+            }
+          }
+      }
+
+      private def selfAssessmentEnrolments(utr: String): Predicate = {
+        (Individual and Enrolment("IR-SA").withIdentifier("UTR", utr)) or
+          (Organisation and Enrolment("IR-SA").withIdentifier("UTR", utr))
+      }
+
+      private def checkForMtdEnrolment(mtdId: String): Predicate = {
+        (Individual and Enrolment("HMRC-MTD-IT").withIdentifier("MTDITID", mtdId)) or
+          (Organisation and Enrolment("HMRC-MTD-IT").withIdentifier("MTDITID", mtdId)) or
+          (Agent and Enrolment("HMRC-AS-AGENT"))
+      }
+
+      private def agentDelegatedEnrolments(utr: String, mtdId: String): Predicate = {
+        Enrolment("HMRC-MTD-IT")
+          .withIdentifier("MTDITID", mtdId)
+          .withDelegatedAuthRule("mtd-it-auth") or
+          Enrolment("IR-SA").withIdentifier("UTR", utr).withDelegatedAuthRule("sa-auth")
       }
     }
-    
-    
-    
-    
-    private def selfAssessmentEnrolments(utr:String): Predicate = {
-      (Individual and Enrolment("IR-SA").withIdentifier("UTR", utr)) or
-        (Individual and checkForMtdEnrolment(utr) )
-    }
 
-    private def checkForMtdEnrolment(utr:String): Predicate ={
-      ???
-    }
-  }
+  object AuthenticateRequestController {}
 }
--- app/models/RequestData.scala
+++ app/models/RequestData.scala
@@ -18,4 +18,4 @@ package models
 
 import play.api.mvc.{Request, WrappedRequest}
 
-case class RequestData[A](utr:String, request:Request[A]) extends WrappedRequest[A](request)
+case class RequestData[A](utr:String, nino:Option[String], request:Request[A]) extends WrappedRequest[A](request)
