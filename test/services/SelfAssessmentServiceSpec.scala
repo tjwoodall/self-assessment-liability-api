e9ed2d85eb594b322eee2e76557e514b338ced8d
aaocs <132440545+aaocs@users.noreply.github.com>
2025-07-14 11:25:08 +0100

    uncomment tests

--- test/services/SelfAssessmentServiceSpec.scala
+++ test/services/SelfAssessmentServiceSpec.scala
@@ -25,7 +25,7 @@ import org.mockito.Mockito.when
 import org.scalatest.prop.TableDrivenPropertyChecks.forEvery
 import org.scalatest.prop.Tables.Table
 import org.scalatestplus.mockito.MockitoSugar.mock
-import play.api.Application
+import play.api.{Application, UnexpectedException}
 import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, ControllerComponents, Result}
@@ -132,92 +132,92 @@ class SelfAssessmentServiceSpec extends SpecBase with HttpWireMock {
         }
       }
 
-//      "return Bad Request for relevant HIP error(s)" in {
-//        when(hipConnector.getSelfAssessmentData(any(), any())(any(), any()))
-//          .thenReturn(Future.failed(No_Payments_Found_For_UTR))
-//
-//        running(app) {
-//          val result = controllerMethod(utr, date, controller)(FakeRequest())
-//
-//          status(result) mustBe BAD_REQUEST
-//          contentAsJson(result) mustBe ApiErrorResponses(
-//            "Bad Request",
-//            "Invalid request format or parameters"
-//          ).asJson
-//        }
-//      }
-//
-//      "return Unauthorized for relevant HIP error(s)" in {
-//        when(hipConnector.getSelfAssessmentData(any(), any())(any(), any()))
-//          .thenReturn(Future.failed(HIP_Unauthorised))
-//
-//        running(app) {
-//          val result = controllerMethod(utr, date, controller)(FakeRequest())
-//
-//          status(result) mustBe UNAUTHORIZED
-//          contentAsJson(result) mustBe ApiErrorResponses(
-//            "Unauthorised",
-//            "Invalid request format or parameters"
-//          ).asJson
-//        }
-//      }
-//
-//      "return Forbidden for relevant HIP error(s)" in {
-//        when(hipConnector.getSelfAssessmentData(any(), any())(any(), any()))
-//          .thenReturn(Future.failed(HIP_Forbidden))
-//
-//        running(app) {
-//          val result = controllerMethod(utr, date, controller)(FakeRequest())
-//
-//          status(result) mustBe FORBIDDEN
-//          contentAsJson(result) mustBe ApiErrorResponses(
-//            "Forbidden",
-//            "Access not permitted"
-//          ).asJson
-//        }
-//      }
-//
-//      "return Internal Server Error for relevant HIP error(s)" in {
-//        forEvery(
-//          Table(
-//            "Internal Server Errors",
-//            Invalid_Correlation_Id,
-//            Invalid_UTR,
-//            HIP_Server_Error,
-//            HIP_Bad_Gateway,
-//            Downstream_Error,
-//            AnyOtherType
-//          )
-//        ) { serviceError =>
-//          when(hipConnector.getSelfAssessmentData(any(), any())(any(), any()))
-//            .thenReturn(Future.failed(serviceError))
-//
-//          running(app) {
-//            val result = controllerMethod(utr, date, controller)(FakeRequest())
-//
-//            status(result) mustBe INTERNAL_SERVER_ERROR
-//            contentAsJson(result) mustBe ApiErrorResponses(
-//              "Internal Server Error",
-//              "Unexpected internal error. Please try again later."
-//            ).asJson
-//          }
-//        }
-//      }
-//
-//      "return Service Unavailable for relevant HIP error(s)" in {
-//        when(hipConnector.getSelfAssessmentData(any(), any())(any(), any()))
-//          .thenReturn(Future.failed(HIP_Service_Unavailable))
-//
-//        running(app) {
-//          val result = controllerMethod(utr, date, controller)(FakeRequest())
-//
-//          status(result) mustBe SERVICE_UNAVAILABLE
-//          contentAsJson(result) mustBe ApiErrorResponses(
-//            "Service Unavailable",
-//            "Service unavailable. Pleased try again later"
-//          ).asJson
-//        }
-//      }
+      "return Bad Request for relevant HIP error(s)" in {
+        when(hipConnector.getSelfAssessmentData(any(), any())(any(), any()))
+          .thenReturn(Future.failed(No_Payments_Found_For_UTR))
+
+        running(app) {
+          val result = selfAssessmentService.getHipData(utr, date)
+
+          status(result) mustBe BAD_REQUEST
+          contentAsJson(result) mustBe ApiErrorResponses(
+            "Bad Request",
+            "Invalid request format or parameters"
+          ).asJson
+        }
+      }
+
+      "return Unauthorized for relevant HIP error(s)" in {
+        when(hipConnector.getSelfAssessmentData(any(), any())(any(), any()))
+          .thenReturn(Future.failed(HIP_Unauthorised))
+
+        running(app) {
+          val result = selfAssessmentService.getHipData(utr, date)
+
+          status(result) mustBe UNAUTHORIZED
+          contentAsJson(result) mustBe ApiErrorResponses(
+            "Unauthorised",
+            "Invalid request format or parameters"
+          ).asJson
+        }
+      }
+
+      "return Forbidden for relevant HIP error(s)" in {
+        when(hipConnector.getSelfAssessmentData(any(), any())(any(), any()))
+          .thenReturn(Future.failed(HIP_Forbidden))
+
+        running(app) {
+          val result = selfAssessmentService.getHipData(utr, date)
+
+          status(result) mustBe FORBIDDEN
+          contentAsJson(result) mustBe ApiErrorResponses(
+            "Forbidden",
+            "Access not permitted"
+          ).asJson
+        }
+      }
+
+      "return Internal Server Error for relevant HIP error(s)" in {
+        forEvery(
+          Table(
+            "Internal Server Errors",
+            Invalid_Correlation_Id,
+            Invalid_UTR,
+            HIP_Server_Error,
+            HIP_Bad_Gateway,
+            Downstream_Error,
+            Throwable()
+          )
+        ) { serviceError =>
+          when(hipConnector.getSelfAssessmentData(any(), any())(any(), any()))
+            .thenReturn(Future.failed(serviceError))
+
+          running(app) {
+          val result = selfAssessmentService.getHipData(utr, date)
+
+            status(result) mustBe INTERNAL_SERVER_ERROR
+            contentAsJson(result) mustBe ApiErrorResponses(
+              "Internal Server Error",
+              "Unexpected internal error. Please try again later."
+            ).asJson
+          }
+        }
+      }
+
+      "return Service Unavailable for relevant HIP error(s)" in {
+        when(hipConnector.getSelfAssessmentData(any(), any())(any(), any()))
+          .thenReturn(Future.failed(HIP_Service_Unavailable))
+
+        running(app) {
+          val result = selfAssessmentService.getHipData(utr, date)
+
+          status(result) mustBe SERVICE_UNAVAILABLE
+          contentAsJson(result) mustBe ApiErrorResponses(
+            "Service Unavailable",
+            "Service unavailable. Pleased try again later"
+          ).asJson
+        }
+      }
     }
   }
 }
